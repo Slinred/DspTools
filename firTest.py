02f374987d0357aac674ab5f8fcfef1f1806857d
nils <nils@christians-web.de>
2022-09-18 19:01:29 +0200

    added functionality for FIR filter design

--- firTest.py
+++ firTest.py
@@ -1,4 +1,11 @@
-from DSPTools import FirFilterFactory
+from DSPTools import FirFilter, FirFilterFactory
 
-f = FirFilterFactory.getLowPassCoefficients(2000,3117,49880)
+coeffs = FirFilterFactory.getLowPassCoefficients(2000,3117,49880, N=80)
+f = FirFilter(coeffs)
 
+print(coeffs)
+print("")
+# Turn to true for use with CMSIS DSP lib!
+f.printCoeffsCArray(True)
+
+input("Press Enter to exit!")
